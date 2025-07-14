9d050f80abdb878c937a9bcaff6830475138a56c
BernardoZocatele <bernardonovaeszocatele@gmail.com>
2025-07-14 19:41:54 -0300

    Estoque padrão

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-import eel # importando biblioteca eel
+import eel 
 import funcoes_banco.gerar_banco
 import funcoes_banco.usuario
 import funcoes_banco.estoque
 import funcoes_banco.logs
+import os
+import sys
+
+def caminho_recurso(relativo):
+    if getattr(sys, 'frozen', False): 
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relativo)
+
+db_path = caminho_recurso("estoque.db")
+web_path = caminho_recurso("web/index.html")
     
 
 eel.init('web') # Diz para o eel qual o nome da psta que irá conter os arquivos web (html, css, java)
